7c7eabcd68944c86bc8089e586668148ab0b5cc7
Zaid Ghanem Khoury <khouryz@circinus-33.ics.uci.edu>
2019-10-30 18:21:21 -0700

    works but loses

--- StudentAI.py
+++ StudentAI.py
@@ -1,6 +1,7 @@
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
+import copy
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 class StudentAI():
@@ -14,14 +15,40 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+
+    def chooseBestMove(self, result):
+        max = -500
+        brd = copy.deepcopy(self.board)
+        bestMove = None
+        for moves in result:
+            for move in moves:
+                #brd.make_move(result[0][0], self.color)
+                brd.make_move(move, self.color)
+                if(self.color == 'B'):
+                    score = brd.black_count - brd.white_count
+                else:
+                    score = brd.white_count - brd.black_count
+                score = score + len(move.seq)*10
+                if score > max:
+                    max = score
+                    bestMove = move
+
+                brd = copy.deepcopy(self.board)
+
+        return bestMove
+
+        
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
-        self.board.make_move(move,self.color)
+        #index = randint(0,len(moves)-1)
+        #inner_index =  randint(0,len(moves[index])-1)
+        #move = moves[index][inneir_index]
+        move = self.chooseBestMove(moves)
+        self.board.make_move(move ,self.color)
         return move
+
+
