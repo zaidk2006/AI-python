d28e286f559b55b83f720286b98d8f8d92f74bed
Kenney Nhu Khiem Phan <kenneyp@circinus-15.ics.uci.edu>
2019-11-12 19:23:09 -0800

    i try pushing new alpha beta

--- StudentAI.py
+++ StudentAI.py
@@ -5,6 +5,7 @@ import copy
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 class StudentAI():
+    movePairs = {}
 
     def __init__(self,col,row,p):
         self.col = col
@@ -19,29 +20,31 @@ class StudentAI():
         self.bestMove = None
         self.MIN = -1000
         self.MAX = 1000
-        self.movePairs = {}
+        #self.movePairs = {}
 
     def getBestMove(self):
-        #print("move pairs: ", self.movePairs)
+        print("for player: ", self.color)
+        print("move pairs: ", self.movePairs)
         bestVal = max(self.movePairs.keys())
         return self.movePairs.get(bestVal)
             	
 
     def minimax(self, board, depth, color, isMaxPlayer, alpha, beta):
         actions = board.get_all_possible_moves(color)
-        tempMoves = copy.deepcopy(actions)
         if depth == 0 or len(actions) == 0:
             return self.boardScore(board, color)
         if isMaxPlayer:
+            #max player is both in self mode since both are using this algorithm
+            #if you print here, it will print for both players as if you are black/white
             bestVal = self.MIN
-            for moves in tempMoves:
+            for moves in actions:
                 for move in moves:
                     tempBoard = copy.deepcopy(board)
                     tempBoard.make_move(move, color)
                     val = self.minimax(tempBoard, depth-1, self.opponent[color], False, alpha, beta)
-                    #getting the wrong move for different board = invalid move?
-                    self.movePairs.update( {val : move} )
-                    print("move pairs in max: ", self.movePairs)
+                    global movePairs
+                    movePairs.update( {val : move} )
+                    print("INSIDE MAX: ", self.movePairs)
                     bestVal = max(bestVal, val)
                     alpha = max(alpha, bestVal)
                     if beta <= alpha:
@@ -49,7 +52,7 @@ class StudentAI():
             return bestVal
         else:
             bestVal = self.MAX
-            for moves in tempMoves:
+            for moves in actions:
                 for move in moves:
                     tempBoard = copy.deepcopy(board)
                     tempBoard.make_move(move, color)
@@ -62,8 +65,6 @@ class StudentAI():
             
 
     def boardScore(self, board, color):
-        #print("black: ", board.black_count)
-        #print("white: ", board.white_count)
         if(color == 1):
             return board.black_count - board.white_count
         elif(color == 2):
@@ -77,13 +78,10 @@ class StudentAI():
         else:
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
-        #index = randint(0,len(moves)-1)
-        #inner_index =  randint(0,len(moves[index])-1)
-        #move = moves[index][inneir_index]
+
         self.minimax(self.board, 3, self.color, True, self.MIN, self.MAX)
-        #print("val: ", val)
         move = self.getBestMove()
-        self.board.make_move(move ,self.color)
+        self.board.make_move(move, self.color)
         return move
 
 
